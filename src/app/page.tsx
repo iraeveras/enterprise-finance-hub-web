5dbadbbfac92c6a1e49353cc412b0a0fb08ec3c5
Irae Veras <irae.online@gmail.com>
2025-07-21 20:48:55 -0300

    feat: HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,50 @@
+"use client"
+import { Header } from "@/components/layout/Header";
+import { MobileSidebar } from "@/components/layout/MobileSidebar";
+import { Sidebar } from "@/components/layout/Sidebar";
 import Image from "next/image";
+import React, { useState } from "react";
+import { DashboardContent } from "./(private)/dashboard/page";
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState("dashboard");
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [user] = useState({
+    name: "Administrador",
+    role: "admin",
+    email: "admin@empresa.com",
+  });
+
+  const renderContent = () => {
+    switch (activeSection) {
+      case "dashboard":
+        return <DashboardContent/>
+      default:
+        return <DashboardContent/>
+    }
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Home</h1>
+    <div className="h-screen bg-gray-50 flex w-full overflow-hidden">
+      {/* Desktop Sidebar */}
+      <Sidebar
+        activeSection={activeSection}
+        onSectionChange={setActiveSection}
+        userRole={user.role}
+      />
+
+      {/* Mobile Sidebar */}
+      <MobileSidebar
+        activeSection={activeSection}
+        onSectionChange={setActiveSection}
+        userRole={user.role}
+        isOpen={mobileMenuOpen}
+        onOpenChange={setMobileMenuOpen}
+      />
+      <div className="flex-1 flex flex-col md:ml-64 overflow-hidden">
+        <Header user={user} onMobileMenuToggle={() => setMobileMenuOpen(true)} />
+        <main className="flex-1 p-4 md:p-6 overflow-y-auto">{renderContent()}</main>
+      </div>
     </div>
   );
 }
