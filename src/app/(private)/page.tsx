b45ddcbee60263cb6c137f106e5cfae4eac250a5
Irae Veras <irae.online@gmail.com>
2025-07-26 13:12:04 -0300

    fix: moved page for private

---
+++ src/app/(private)/page.tsx
@@ -0,0 +1,57 @@
+// FILE: src/app/page.tsx
+"use client"
+import { Header } from "@/components/layout/Header";
+import { MobileSidebar } from "@/components/layout/MobileSidebar";
+import { Sidebar } from "@/components/layout/Sidebar";
+import Image from "next/image";
+import React, { useState } from "react";
+import { DashboardContent } from "./dashboard/page";
+import CompanyManager from "./companies/page";
+import { ProtectedPage } from "@/components/layout/ProtectedPage";
+
+export default function Home() {
+  const [activeSection, setActiveSection] = useState("dashboard");
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [user] = useState({
+    name: "Administrador",
+    role: "admin",
+    email: "admin@empresa.com",
+  });
+
+  const renderContent = () => {
+    switch (activeSection) {
+      case "dashboard":
+        return <DashboardContent/>
+      case "companies":
+        return <CompanyManager />;
+      default:
+        return <DashboardContent/>
+    }
+  }
+
+  return (
+    <ProtectedPage>
+      <div className="h-screen bg-gray-50 flex w-full overflow-hidden">
+        {/* Desktop Sidebar */}
+        <Sidebar
+          activeSection={activeSection}
+          onSectionChange={setActiveSection}
+          userRole={user.role}
+        />
+
+        {/* Mobile Sidebar */}
+        <MobileSidebar
+          activeSection={activeSection}
+          onSectionChange={setActiveSection}
+          userRole={user.role}
+          isOpen={mobileMenuOpen}
+          onOpenChange={setMobileMenuOpen}
+        />
+        <div className="flex-1 flex flex-col md:ml-64 overflow-hidden">
+          <Header user={user} onMobileMenuToggle={() => setMobileMenuOpen(true)} />
+          <main className="flex-1 p-4 md:p-6 overflow-y-auto">{renderContent()}</main>
+        </div>
+      </div>
+    </ProtectedPage>
+  );
+}
