271ad81513ddfad56359fe6127d3e26f8ef2e06e
Irae Veras <irae.online@gmail.com>
2025-07-29 00:04:24 -0300

    fix: added news interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,18 +8,50 @@ export interface Company {
     updatedAt: string;
 }
 
+export interface CompanyFormData {
+    cnpj: string;
+    corporateName: string;
+    tradeName: string;
+    status: 'active' | 'inactive';
+}
+
+export type ModuleID =
+    | "companies"
+    | "employees"
+    | "departments"
+    | "sectors"
+    | "teams"
+    | "costcenters"
+    | "budget"
+    | "reports"
+    | "schedule"
+    | "users";
+
+export type RoleLevel = "admin" | "manager" | "coordinator" | "supervisor";
+
+export type Action = "read" | "write" | "delete" | "export";
+
+export type Scope = "all" | "company" | "department" | "sector";
+
+export interface Permission {
+    module: ModuleID;
+    actions: Action[];
+    scope: Scope;
+}
+
+export interface FormDataRole {
+    name: string;
+    level: RoleLevel;
+    description: string;
+    permissions: Permission[];
+}
+
 export interface Role {
     id: string;
     name: string;
     permissions: Permission[];
-    level: 'admin' | 'manager' | 'coordinator' | 'supervisor';
+    level: RoleLevel;
     description: string;
-}
-
-export interface Permission {
-    module: string;
-    actions: ('read' | 'write' | 'delete' | 'export')[];
-    scope: 'all' | 'company' | 'department' | 'sector';
 }
 
 export interface User {
@@ -31,6 +63,35 @@ export interface User {
     status: 'active' | 'inactive';
     createdAt: string;
     lastLogin?: string;
+}
+
+export interface FormDataUser {
+    name: string;
+    email: string;
+    role: Role;
+    companies: Company[];
+    status: 'active' | 'inactive';
+    createdAt: string;
+    lastLogin?: string;
+}
+
+export interface CreateUserInput {
+    name: string;
+    email: string;
+    password: string;
+    roleId: number;
+    companyIds: number[];
+    status: "active" | "inactive";
+}
+export interface UpdateUserInput {
+    id: string;
+    name: string;
+    email: string;
+    // password opcional
+    password?: string;
+    roleId: number;
+    companyIds: number[];
+    status: "active" | "inactive";
 }
 
 export interface Department {
