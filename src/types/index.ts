994586049c6b0552804f5f9bf945607d24368b21
Irae Veras <irae.online@gmail.com>
2025-07-29 22:47:38 -0300

    fix: added type Department

--- src/types/index.ts
+++ src/types/index.ts
@@ -97,9 +97,14 @@ export interface UpdateUserInput {
 export interface Department {
     id: string;
     name: string;
-    companyId: string;
+    companyId: number;
     status: 'active' | 'inactive';
+    createdAt: string;
+    updatedAt: string;
 }
+
+export type CreateDepartmentInput = Omit<Department, "id" | "createdAt" | "updatedAt">;
+export type UpdateDepartmentInput = Department;
 
 export interface Sector {
     id: string;
